d18c86675f25c424879bafe8e2a5e64d76c52ef4
Gokcimen <77895695+Gokcimen@users.noreply.github.com>
2022-06-12 17:58:43 +0300

    Son surum eklendi
    Son surum eklendi

---
+++ Güvenli Dijital Hayat Projesi/repos/RandomNumberGenerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static StreamWriter Yaz = null;
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Yaz = new StreamWriter(@"C:\Users\TUNAHAN\source\repos\RandomNumberGenerator\data2.txt");
+                for (int i = 0; i < 1000000; i++)
+                {
+                    //Console.Write(RandomNumberGenerator.Instance.Generate(0, 1)+" ");
+
+                    Yaz.Write(RandomNumberGenerator.Instance.Generate(0, 1) + ";");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            finally
+            {
+                Yaz.Flush();
+                Yaz.Close();
+
+            }
+
+
+
+
+        }
+    }
+}
